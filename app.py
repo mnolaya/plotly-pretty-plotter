a6ba2ddac895937e4b1f6226c5d37a1f819dc934
Michael Olaya <michael.n.olaya@gmail.com>
2022-12-09 10:13:16 -0500

    Added sidebar to layout.

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from dash import Dash, dcc, dash_table
 import dash_bootstrap_components as dbc
 
+from pages.sidebar import sidebar
+
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(sidebar)
+    ])
+])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
